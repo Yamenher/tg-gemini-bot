f520cf6b27028a6421c2f0e6ce525c78221cd0cb
Yamen <164387611+Yamenher@users.noreply.github.com>
2024-10-10 11:19:08 +0100

    Update gemini.py

--- api/gemini.py
+++ api/gemini.py
@@ -9,6 +9,7 @@ genai.configure(api_key=GOOGLE_API_KEY[0])
 
 model_usual = genai.GenerativeModel(
     model_name="gemini-pro",
+    system_instruction="Toxic non friendly bot",
     generation_config=generation_config,
     safety_settings=safety_settings)
 
